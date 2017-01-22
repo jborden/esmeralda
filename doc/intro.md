b716839b53520b15e7a2f385c870aa4f77c3536d
James <jmborden@gmail.com>
2017-01-21 22:05:47 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to esmeralda
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/esmeralda/core_test.clj
@@ -0,0 +1,7 @@
+(ns esmeralda.core-test
+  (:require [clojure.test :refer :all]
+            [esmeralda.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
